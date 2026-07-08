30d1ff6cd3871b08c4eea8ad1e8d3fb143379321
Moinak Ghosh <216645880+moinakgh99@users.noreply.github.com>
2026-07-08 19:00:18 +0530

    Time: 43 ms (55.44%) | Memory: 155.3 MB (63.21%) - LeetSync

--- 4136-concatenate-non-zero-digits-and-multiply-by-sum-ii/concatenate-non-zero-digits-and-multiply-by-sum-ii.cpp
+++ 4136-concatenate-non-zero-digits-and-multiply-by-sum-ii/concatenate-non-zero-digits-and-multiply-by-sum-ii.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
 
     typedef long long ll;
-    int MOD = 1e9+7;
+    int mod = 1e9+7;
     vector<int> sumAndMultiply(string s, vector<vector<int>>& queries) {
         
         // TLE Coz of O(n2) ....
@@ -29,64 +29,54 @@ public:
 
 
         // Optimal Approach ...
-         int n = s.length();
+        int n = s.length();
+        vector<ll> digSum(n,0); // to calculate the sum upto last
+        vector<int> nonZeroDig(n,0); // to count no. of non dig element
+        vector<ll> pow10(n+1, 0); // to calculate the power of 10
+        vector<ll> numberFormed(n, 0);  // to check the number formed upto last
 
-        vector<int> nonZeroCount(n, 0);    // non-zero digits count in s[0..i]
-        vector<ll> numberUpTo(n, 0);      // number formed from non-zero digits in s[0..i]
-        vector<ll> digitSumUpTo(n, 0);    // digit sum of s[0..i]
-        vector<ll> pow10(n + 1, 0);  // 10^i
-
-        pow10[0] = 1;
-        for (int i = 1; i <= n; i++)
-            pow10[i] = (pow10[i - 1] * 10) % MOD;
-
-
-        nonZeroCount[0]      = (s[0] != '0') ? 1 : 0;
-        numberUpTo[0]   = s[0] - '0';
-        digitSumUpTo[0] = s[0] - '0';
-
-        for (int i = 1; i < n; i++) {
-            int digit = s[i] - '0';
-            nonZeroCount[i] = nonZeroCount[i - 1] + (digit != 0 ? 1 : 0);
+        pow10[0] = 1;  // 10 ki power 0 hoti h 1, pehle hi push kra dia
+        for(int i = 1; i <= n; i++) {
+            pow10[i] = (1LL * pow10[i-1] * 10) % mod;
         }
 
-        for (int i = 1; i < n; i++) {
-            int digit = s[i] - '0';
-            if (digit != 0)
-                numberUpTo[i] = (numberUpTo[i - 1] * 10 + digit) % MOD;
-            else
-                numberUpTo[i] = numberUpTo[i - 1];
+        nonZeroDig[0] = (s[0] != '0') ? 1 : 0;
+        for(int i =1; i < n; i++) {
+            int dig = s[i] - '0';
+            nonZeroDig[i] = nonZeroDig[i-1] + ((dig != 0) ? 1 : 0);
         }
 
-        for (int i = 1; i < n; i++) {
-            digitSumUpTo[i] = digitSumUpTo[i - 1] + (s[i] - '0');
-        }
-
-        int q = queries.size();
-        vector<int> result(q);
-
-        for (int i = 0; i < q; i++) {
-            int l  = queries[i][0];
-            int r = queries[i][1];
-
-            int startCount = (l == 0) ? 0 : nonZeroCount[l - 1];
-            ll numBefore   = (l == 0) ? 0 : numberUpTo[l - 1];
-
-            int endCount = nonZeroCount[r];
-            int subStrLen = endCount - startCount;
-
-            if (subStrLen == 0) {
-                result[i] = 0;
-                continue;
+        numberFormed[0] = s[0] - '0'; // bss pehla dig push kia h !!
+        for(int i = 1; i < n; i++) {
+            int dig = s[i] - '0';  // dig nikalo h !!
+            if(dig != 0) {
+                numberFormed[i] = ((numberFormed[i-1] * 10) + dig) % mod;
             }
-
-            ll x   = (numberUpTo[r] - (numBefore * pow10[subStrLen] % MOD) + MOD) % MOD;
-            ll sum = digitSumUpTo[r] - ((l == 0) ? 0 : digitSumUpTo[l - 1]);
-
-            result[i] = (int)((x * sum) % MOD);
+            else numberFormed[i] = numberFormed[i-1];  // agar dig 0 rh to !!
         }
 
-        return result;
+        digSum[0] = s[0] - '0';
+        for(int i = 1; i < n; i++) {
+            int dig = s[i] - '0';
+            digSum[i] = digSum[i-1] + dig;
+        }
+
+        int m = queries.size();
+        vector<int> res(m);
+
+        for(int i = 0; i < m; i++) {
+            int l = queries[i][0], r = queries[i][1];
+
+            ll sum = digSum[r] - ((l == 0) ? 0 : digSum[l-1]);
+            
+            int number = (l == 0) ? 0 : numberFormed[l-1];
+            int k = nonZeroDig[r] - ((l == 0) ? 0 : nonZeroDig[l-1]);
+            ll x = (numberFormed[r] - (number * pow10[k] * 1ll % mod) + mod) % mod;
+
+            res[i] = (int)((x * sum) % mod);
+        }
+
+        return res;
 
     }
 };
