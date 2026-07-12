e9eae0778beed679bb48dd5c2c7ba2d0e1e5b977
Moinak Ghosh <216645880+moinakgh99@users.noreply.github.com>
2026-07-12 18:59:17 +0530

    Time: 0 ms (100.00%) | Memory: 17.5 MB (78.31%) - LeetSync

--- 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.cpp
+++ 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.cpp
@@ -13,28 +13,49 @@ class Solution {
 public:
 
     // My Approach , preorder traversal nodes are stored in vector, then loop throgh vector to connect them ..
+
     vector<TreeNode*> v;
-    void preorder(TreeNode* root) {
-        if(root == NULL) return;
+    // void preorder(TreeNode* root) {
+    //     if(root == NULL) return;
 
-        v.push_back(root);
-        preorder(root->left);
-        preorder(root->right);
-    }
+    //     v.push_back(root);
+    //     preorder(root->left);
+    //     preorder(root->right);
+    // }
 
+    // void flatten(TreeNode* root) {
+    //     if(root == NULL) return;
+
+    //     preorder(root);
+
+    //     for(int i = 1; i < v.size(); i++) {
+    //         v[i - 1]->left = NULL;
+    //         v[i - 1]->right = v[i];
+    //     }
+
+    //     if(!v.empty()) {
+    //         v.back()->left = NULL;
+    //         v.back()->right = NULL;
+    //     }
+    // }
+
+
+    // Most Optimal Approach ... O(1) space
+    /*
+        Preorder ko reverse me traverse kro , pre - VLR
+        but in this case , hum ulta krenga , RLV
+        NextPointer -> last visited node ko point krega ...
+    */
+
+    TreeNode *nextPointer = NULL;
     void flatten(TreeNode* root) {
         if(root == NULL) return;
 
-        preorder(root);
+        flatten(root->right);
+        flatten(root->left);
 
-        for(int i = 1; i < v.size(); i++) {
-            v[i - 1]->left = NULL;
-            v[i - 1]->right = v[i];
-        }
-
-        if(!v.empty()) {
-            v.back()->left = NULL;
-            v.back()->right = NULL;
-        }
+        root->left = NULL;
+        root->right = nextPointer;
+        nextPointer = root;
     }
 };
