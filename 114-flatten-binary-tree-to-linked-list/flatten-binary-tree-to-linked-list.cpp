89a16391bde057f854f65d3a999ba7b73b0e115d
Moinak Ghosh <216645880+moinakgh99@users.noreply.github.com>
2026-07-12 18:24:47 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (15.92%) - LeetSync

---
+++ 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.cpp
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+
+    // My Approach , preorder traversal nodes are stored in vector, then loop throgh vector to connect them ..
+    vector<TreeNode*> v;
+    void preorder(TreeNode* root) {
+        if(root == NULL) return;
+
+        v.push_back(root);
+        preorder(root->left);
+        preorder(root->right);
+    }
+
+    void flatten(TreeNode* root) {
+        if(root == NULL) return;
+
+        preorder(root);
+
+        for(int i = 1; i < v.size(); i++) {
+            v[i - 1]->left = NULL;
+            v[i - 1]->right = v[i];
+        }
+
+        if(!v.empty()) {
+            v.back()->left = NULL;
+            v.back()->right = NULL;
+        }
+    }
+};
