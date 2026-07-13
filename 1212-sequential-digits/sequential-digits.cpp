a83f9ed6f22f7704a7a8509dc931e285a5dd9b83
Moinak Ghosh <216645880+moinakgh99@users.noreply.github.com>
2026-07-13 19:19:21 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (48.96%) - LeetSync

--- 1212-sequential-digits/sequential-digits.cpp
+++ 1212-sequential-digits/sequential-digits.cpp
@@ -4,20 +4,43 @@ public:
         
 
         // Approach 1 : Using BFS ....
-        queue<int> q;
-        for(int i = 1; i <= 8; i++) {
-            q.push(i);
-        }
+        // queue<int> q;
+        // for(int i = 1; i <= 8; i++) {
+        //     q.push(i);
+        // }
+
+        // vector<int> res;
+        // while(!q.empty()) {
+        //     int num = q.front();
+        //     q.pop();
+
+        //     if(num >= low && num <= high) res.push_back(num);
+
+        //     int dig = num % 10;
+        //     if((dig + 1) <= 9) q.push(num * 10 + (dig + 1));
+        // }
+
+        // return res;
+
+
+        // Approach 2 : Simply store all the result in a vector .
+        vector<int> allPossible = {12, 23, 34, 45, 56, 67, 78, 89,
+                          123, 234, 345, 456, 567, 678, 789,
+                          1234, 2345, 3456, 4567, 5678, 6789,
+                          12345, 23456, 34567, 45678, 56789,
+                          123456, 234567, 345678, 456789,
+                          1234567, 2345678, 3456789,
+                          12345678, 23456789,
+                          123456789
+        };
 
         vector<int> res;
-        while(!q.empty()) {
-            int num = q.front();
-            q.pop();
 
-            if(num >= low && num <= high) res.push_back(num);
+        for(int i = 0; i < allPossible.size(); i++) {
+            if(allPossible[i] < low) continue;
+            if(allPossible[i] > high) break;
 
-            int dig = num % 10;
-            if((dig + 1) <= 9) q.push(num * 10 + (dig + 1));
+            res.push_back(allPossible[i]);
         }
 
         return res;
