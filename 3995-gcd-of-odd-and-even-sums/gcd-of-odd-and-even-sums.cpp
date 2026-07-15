0534f8641f377e6af9cbb13eb27f671233b19e72
Moinak Ghosh <216645880+moinakgh99@users.noreply.github.com>
2026-07-15 19:20:33 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (85.02%) - LeetSync

--- 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.cpp
+++ 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.cpp
@@ -1,12 +1,21 @@
 class Solution {
 public:
     int gcdOfOddEvenSums(int n) {
-        int sumEven = 0, sumOdd = 0;
-        for(int i = 1; i <= 2*n; i++) {
-            if(i % 2 == 0) sumEven += i;
-            else sumOdd += i;
-        }
 
-        return __gcd(sumEven, sumOdd);
+        // Approach 1 .... Simple for Loop
+        // int sumEven = 0, sumOdd = 0;
+        // for(int i = 1; i <= 2*n; i++) {
+        //     if(i % 2 == 0) sumEven += i;
+        //     else sumOdd += i;
+        // }
+
+        // return __gcd(sumEven, sumOdd);
+
+
+        // Approach 2 : Mathematical Formula ...
+        int sumOdd = n * n;
+        int sumEven = n * ( n + 1 );
+
+        return __gcd(sumOdd, sumEven);
     }
 };
