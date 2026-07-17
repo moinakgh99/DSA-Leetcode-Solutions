073dc37e03d73e8d473128110d8f4330aeeb939c
Moinak Ghosh <216645880+moinakgh99@users.noreply.github.com>
2026-07-17 18:22:54 +0530

    Time: 0 ms (100.00%) | Memory: 26.3 MB (6.59%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -3,10 +3,36 @@ public:
     int singleNonDuplicate(vector<int>& nums) {
         
         // Approach 1 : O(n) time and space
-        unordered_map<int,int> mp;
-        for(int x : nums) mp[x]++;
-        for(auto &it : mp) {
-            if(it.second == 1) return it.first;
+        // unordered_map<int,int> mp;
+        // for(int x : nums) mp[x]++;
+        // for(auto &it : mp) {
+        //     if(it.second == 1) return it.first;
+        // }
+        // return -1;
+
+
+        // Approach 2 : The question asked ...
+        int n = nums.size();
+        int start = 0, end = n - 1;
+
+        if(n == 1) return nums[0];
+
+        while(start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if(mid == 0 && nums[0] != nums[1]) return nums[mid];
+            if(mid == n-1 && nums[mid] != nums[mid - 1]) return nums[mid];
+            
+            if(nums[mid] != nums[mid - 1] && nums[mid] != nums[mid + 1]) return nums[mid];
+
+            if(mid % 2 == 0) {  // agar mid even ho ...
+                if(nums[mid] == nums[mid - 1]) end = mid - 1;
+                else start = mid + 1;
+            }
+            else {
+                if(nums[mid] == nums[mid - 1]) start = mid + 1;
+                else end = mid - 1;
+            }
         }
         return -1;
     }
