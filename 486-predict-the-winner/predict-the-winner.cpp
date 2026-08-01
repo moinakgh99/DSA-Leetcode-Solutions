0f39b5489e1c50ae5795c68fc07bde0b732c6d22
Moinak Ghosh <216645880+moinakgh99@users.noreply.github.com>
2026-08-01 18:48:04 +0530

    Time: 83 ms (24.73%) | Memory: 9.6 MB (69.71%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.cpp
+++ 486-predict-the-winner/predict-the-winner.cpp
@@ -25,6 +25,8 @@ public:
 
 
     // Memoization of recursive Approach 1 .....
+
+    /*
     int dp[23][23];
     int solve(int i, int j, vector<int>& nums) {
         if(i > j) return 0;
@@ -43,5 +45,38 @@ public:
         memset(dp, -1, sizeof(dp));
 
         return solve(0, n-1, nums) >= 0;
+    }
+    */
+
+
+
+    // Recursive Approach 2 ....
+    int solve(int i, int j, vector<int>& nums) {
+        if(i > j) return 0;
+        if(i == j) return nums[i];
+
+        /* 
+            simply player 1 ko dekh k chlo, ignore player 2 now, vo sbse pehle apna best choose krega, 
+            then min dekhega jb player 1 ki dubara turn aaega , coz player 2 tb tk apna best choose
+            kr chuka hoga !!!
+        */
+        int pick_i = nums[i] + min(solve(i+2, j, nums), solve(i+1, j-1, nums));
+        int pick_j = nums[j] + min(solve(i+1, j-1, nums), solve(i, j-2, nums));
+
+        return max(pick_i, pick_j);
+    }
+
+    bool predictTheWinner(vector<int>& nums) {
+        int n = nums.size();
+        int total_score = 0;
+
+        for(int i = 0; i < n; i++) {
+            total_score += nums[i];
+        }
+
+        int player1_score = solve(0, n-1, nums);
+        int player2_score = total_score - player1_score;
+
+        return player1_score >= player2_score;
     }
 };
