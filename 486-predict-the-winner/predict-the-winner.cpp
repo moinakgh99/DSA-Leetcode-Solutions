476662bf3a879d9d40781951417e4e9f5f501351
Moinak Ghosh <216645880+moinakgh99@users.noreply.github.com>
2026-08-01 18:26:25 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (96.48%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.cpp
+++ 486-predict-the-winner/predict-the-winner.cpp
@@ -2,7 +2,9 @@ class Solution {
 public:
 
     // Recursive Approach 1 .....
-    int solve(uint i, int j, vector<int>& nums) {
+
+    /*
+    int solve(int i, int j, vector<int>& nums) {
         if(i > j) return 0;
         if(i == j) return nums[i];
 
@@ -15,6 +17,30 @@ public:
 
     bool predictTheWinner(vector<int>& nums) {
         int n = nums.size();
+
+        return solve(0, n-1, nums) >= 0;
+    }
+    */
+
+
+
+    // Memoization of recursive Approach 1 .....
+    int dp[23][23];
+    int solve(int i, int j, vector<int>& nums) {
+        if(i > j) return 0;
+        if(i == j) return nums[i];
+        if(dp[i][j] != -1) return dp[i][j];
+
+        // simply diff between player 1 and player 2 ....
+        int pick_i = nums[i] - solve(i+1, j, nums);
+        int pick_j = nums[j] - solve(i, j-1, nums);
+
+        return dp[i][j] = max(pick_i, pick_j);
+    }
+
+    bool predictTheWinner(vector<int>& nums) {
+        int n = nums.size();
+        memset(dp, -1, sizeof(dp));
 
         return solve(0, n-1, nums) >= 0;
     }
