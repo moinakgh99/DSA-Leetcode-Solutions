7b420586f792424e0af5df42145df88115c09068
Moinak Ghosh <216645880+moinakgh99@users.noreply.github.com>
2026-08-01 18:54:35 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (85.12%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.cpp
+++ 486-predict-the-winner/predict-the-winner.cpp
@@ -51,9 +51,45 @@ public:
 
 
     // Recursive Approach 2 ....
+  
+    // int solve(int i, int j, vector<int>& nums) {
+    //     if(i > j) return 0;
+    //     if(i == j) return nums[i];
+
+    //     /* 
+    //         simply player 1 ko dekh k chlo, ignore player 2 now, vo sbse pehle apna best choose krega, 
+    //         then min dekhega jb player 1 ki dubara turn aaega , coz player 2 tb tk apna best choose
+    //         kr chuka hoga !!!
+    //     */
+    //     int pick_i = nums[i] + min(solve(i+2, j, nums), solve(i+1, j-1, nums));
+    //     int pick_j = nums[j] + min(solve(i+1, j-1, nums), solve(i, j-2, nums));
+
+    //     return max(pick_i, pick_j);
+    // }
+
+    /*
+    bool predictTheWinner(vector<int>& nums) {
+        int n = nums.size();
+        int total_score = 0;
+
+        for(int i = 0; i < n; i++) {
+            total_score += nums[i];
+        }
+
+        int player1_score = solve(0, n-1, nums);
+        int player2_score = total_score - player1_score;
+
+        return player1_score >= player2_score;
+    }
+    */
+
+
+    // Memoization of recursive Approach 2 .....
+    int dp[23][23];
     int solve(int i, int j, vector<int>& nums) {
         if(i > j) return 0;
         if(i == j) return nums[i];
+        if(dp[i][j] != -1) return dp[i][j];
 
         /* 
             simply player 1 ko dekh k chlo, ignore player 2 now, vo sbse pehle apna best choose krega, 
@@ -63,11 +99,12 @@ public:
         int pick_i = nums[i] + min(solve(i+2, j, nums), solve(i+1, j-1, nums));
         int pick_j = nums[j] + min(solve(i+1, j-1, nums), solve(i, j-2, nums));
 
-        return max(pick_i, pick_j);
+        return dp[i][j] = max(pick_i, pick_j);
     }
 
     bool predictTheWinner(vector<int>& nums) {
         int n = nums.size();
+        memset(dp, -1, sizeof(dp));
         int total_score = 0;
 
         for(int i = 0; i < n; i++) {
