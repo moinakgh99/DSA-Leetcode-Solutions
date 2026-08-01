116da9a6a1473488d48c5f640ff0a297d416305e
Moinak Ghosh <216645880+moinakgh99@users.noreply.github.com>
2026-08-01 18:23:31 +0530

    Time: 88 ms (22.73%) | Memory: 9.7 MB (56.39%) - LeetSync

---
+++ 486-predict-the-winner/predict-the-winner.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+
+    // Recursive Approach 1 .....
+    int solve(uint i, int j, vector<int>& nums) {
+        if(i > j) return 0;
+        if(i == j) return nums[i];
+
+        // simply diff between player 1 and player 2 ....
+        int pick_i = nums[i] - solve(i+1, j, nums);
+        int pick_j = nums[j] - solve(i, j-1, nums);
+
+        return max(pick_i, pick_j);
+    }
+
+    bool predictTheWinner(vector<int>& nums) {
+        int n = nums.size();
+
+        return solve(0, n-1, nums) >= 0;
+    }
+};
