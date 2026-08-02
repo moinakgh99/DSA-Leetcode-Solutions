3cf4329eafa0a4ce6fc573273e45a5e68d44b0ee
Moinak Ghosh <216645880+moinakgh99@users.noreply.github.com>
2026-08-02 09:46:01 +0530

    Time: 3 ms (38.73%) | Memory: 11.6 MB (36.14%) - LeetSync

---
+++ 909-stone-game/stone-game.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int dp[501][501];
+
+    // memoization of Recursive Approach 1 .... (same as question predict the winner) ....
+    int solve(int i, int j, vector<int>& piles) {
+        if(i > j) return 0;
+        if(i == j) return piles[i];
+
+        if(dp[i][j] != -1) return dp[i][j];
+
+        int pick_i = piles[i] - solve(i+1, j, piles);
+        int pick_j = piles[j] - solve(i, j-1, piles);
+
+        return dp[i][j] = max(pick_i, pick_j);
+    }
+
+    bool stoneGame(vector<int>& piles) {
+        int n = piles.size();
+        memset(dp, -1, sizeof(dp));
+
+        return solve(0, n-1, piles) > 0;
+    }
+};
