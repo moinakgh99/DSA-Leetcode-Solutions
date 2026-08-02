6ab396037413df5e0dc3e1ad1f961aa31c83fa7c
Moinak Ghosh <216645880+moinakgh99@users.noreply.github.com>
2026-08-02 19:56:00 +0530

    Time: 11 ms (27.05%) | Memory: 11.4 MB (37.34%) - LeetSync

--- 909-stone-game/stone-game.cpp
+++ 909-stone-game/stone-game.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     int dp[501][501];
 
+    /*
     // memoization of Recursive Approach 1 .... (same as question predict the winner) ....
     int solve(int i, int j, vector<int>& piles) {
         if(i > j) return 0;
@@ -9,6 +10,7 @@ public:
 
         if(dp[i][j] != -1) return dp[i][j];
 
+        // alice - bob .... (diff btw them)
         int pick_i = piles[i] - solve(i+1, j, piles);
         int pick_j = piles[j] - solve(i, j-1, piles);
 
@@ -20,5 +22,35 @@ public:
         memset(dp, -1, sizeof(dp));
 
         return solve(0, n-1, piles) > 0;
+    }
+    */
+
+
+    // Memoization of recursive approach 2 ....
+
+    int solve(int i, int j, vector<int>& piles) {
+        if(i > j) return 0;
+        if(i == j) return piles[i];
+
+        if(dp[i][j] != -1) return dp[i][j];
+
+        // taking case only for alice
+        int pick_i = piles[i] + min(solve(i+2, j, piles), solve(i+1, j-1, piles));
+        int pick_j = piles[j] + min(solve(i+1, j-1, piles), solve(i, j-2, piles));
+
+        return dp[i][j] = max(pick_i, pick_j);
+    }
+
+    bool stoneGame(vector<int>& piles) {
+        int n = piles.size();
+        memset(dp, -1, sizeof(dp));
+
+        int total = 0;
+        for(int x : piles) total += x;
+
+        int alice = solve(0, n-1, piles);
+        int bob = total - alice;
+
+        return alice > bob;
     }
 };
