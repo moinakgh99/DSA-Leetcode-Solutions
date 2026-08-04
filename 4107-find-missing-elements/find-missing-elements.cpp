6c8084f9e5c7d757991c49f2a10712ed3362759d
Moinak Ghosh <216645880+moinakgh99@users.noreply.github.com>
2026-08-04 17:28:59 +0530

    Time: 10 ms (7.46%) | Memory: 33.8 MB (8.08%) - LeetSync

--- 4107-find-missing-elements/find-missing-elements.cpp
+++ 4107-find-missing-elements/find-missing-elements.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
 
-    // Approach 1 ..... 
+    // Approach 1 ..... using freq array ...
+    /*
     vector<int> findMissingElements(vector<int>& nums) {
         int n = nums.size();
 
@@ -15,6 +16,26 @@ public:
 
         for(int i = mn; i <= mx; i++) {
             if(dig[i] == 0) res.push_back(i);
+        }
+
+        return res;
+    }
+    */
+
+
+    // Approach 2 ... using hashset ....
+    vector<int> findMissingElements(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> res;
+
+        int mx = *max_element(nums.begin(), nums.end());
+        int mn = *min_element(nums.begin(), nums.end());
+
+        unordered_set<int> s;
+        for(int x : nums) s.insert(x);
+
+        for(int i = mn; i <= mx; i++) {
+            if(s.find(i) == s.end()) res.push_back(i);
         }
 
         return res;
