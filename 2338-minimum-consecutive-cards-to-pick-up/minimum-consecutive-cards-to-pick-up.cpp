ee66ecf18b626408c03ad1a4befb1ef45c7ae125
Moinak Ghosh <216645880+moinakgh99@users.noreply.github.com>
2026-08-16 20:20:43 +0530

    Time: 117 ms (91.80%) | Memory: 119.1 MB (62.67%) - LeetSync

--- 2338-minimum-consecutive-cards-to-pick-up/minimum-consecutive-cards-to-pick-up.cpp
+++ 2338-minimum-consecutive-cards-to-pick-up/minimum-consecutive-cards-to-pick-up.cpp
@@ -3,6 +3,9 @@ public:
     int minimumCardPickup(vector<int>& cards) {
 
         // Using sliding Window .. coz finding the consecutive cards 
+        // Approach 1 ..
+
+        /*
         int n = cards.size();
         int i = 0, j = 0;
         int len = INT_MAX;
@@ -22,6 +25,23 @@ public:
 
             st.insert(cards[j]);
             j++;
+        }
+
+        return (len == INT_MAX) ? -1 : len;
+        */
+
+
+        // Approach 2 : Using HashMap -> just simply store krenga konsa element kis idx me present h !!
+        int n = cards.size();
+        int len = INT_MAX;
+        unordered_map<int,int> mp;
+
+        for(int i = 0; i < n; i++) {
+            if(mp.count(cards[i])) {
+                len = min(len, i - mp[cards[i]] + 1);
+            }
+
+            mp[cards[i]] = i;
         }
 
         return (len == INT_MAX) ? -1 : len;
