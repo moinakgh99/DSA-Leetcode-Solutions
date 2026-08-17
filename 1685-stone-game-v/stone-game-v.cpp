f83b9cab633a5f876a8b501703221eed105213db
Moinak Ghosh <216645880+moinakgh99@users.noreply.github.com>
2026-08-17 17:59:33 +0530

    Time: 1560 ms (5.03%) | Memory: 27.8 MB (21.68%) - LeetSync

--- 1685-stone-game-v/stone-game-v.cpp
+++ 1685-stone-game-v/stone-game-v.cpp
@@ -41,6 +41,7 @@ public:
 
     // Memoization of Approach 1 ...
 
+    /*
     int dp[501][501];
 
     int alice(int left, int right, vector<int> &prefix) {
@@ -53,7 +54,7 @@ public:
 
         for(int mid = 0; mid < right; mid++) {
             lSum = prefix[mid] - ((left > 0) ? prefix[left - 1] : 0);  // l .... mid
-            rSum = prefix[right] - prefix[mid];  // mid .... right
+            rSum = prefix[right] - prefix[mid];  // mid+1 .... right
 
             if(lSum < rSum) score = max(score, lSum + alice(left, mid, prefix));
             else if(rSum < lSum) score = max(score, rSum + alice(mid+1, right, prefix));
@@ -76,5 +77,42 @@ public:
         }
 
         return alice(0, n-1, prefix);
+    }
+    */
+
+
+    // Approach 3 : Bottom Up approach ( Tabulation ) ...
+
+    int stoneGameV(vector<int>& stoneValue) {
+        n = stoneValue.size();
+
+        vector<int> prefix(n);
+        prefix[0] = stoneValue[0];
+
+        for(int i = 1; i < n; i++) {
+            prefix[i] = prefix[i-1] + stoneValue[i];
+        }
+
+        vector<vector<int>> dp(n+1, vector<int>(n, 0));
+
+        for(int left = n-1; left >= 0; left--) {
+            for(int right = left+1; right < n; right++) {
+
+                int score = 0;
+                for(int mid = 0; mid < right; mid++) {
+                    int lSum = prefix[mid] - ((left > 0) ? prefix[left - 1] : 0);  // l .... mid
+                    int rSum = prefix[right] - prefix[mid];  // mid+1 .... right
+
+                    if(lSum < rSum) score = max(score, lSum + dp[left][mid]);
+                    else if(rSum < lSum) score = max(score, rSum + dp[mid+1][right]);
+                    else score = max({score, lSum + dp[left][mid], rSum + dp[mid+1][right]});
+
+                }
+
+                dp[left][right] = score;
+            }
+        }
+
+        return dp[0][n-1];
     }
 };
