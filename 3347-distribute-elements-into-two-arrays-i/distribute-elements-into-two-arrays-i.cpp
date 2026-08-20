4a586881418b3823d5a2514c91a3019acd9c5a6e
Moinak Ghosh <216645880+moinakgh99@users.noreply.github.com>
2026-08-20 16:21:21 +0530

    Time: 3 ms (23.77%) | Memory: 24 MB (30.72%) - LeetSync

--- 3347-distribute-elements-into-two-arrays-i/distribute-elements-into-two-arrays-i.cpp
+++ 3347-distribute-elements-into-two-arrays-i/distribute-elements-into-two-arrays-i.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
     vector<int> resultArray(vector<int>& nums) {
+
+        // Simple approach ...
+
+        /*
         int n = nums.size();
         vector<int> arr1, arr2;
         arr1.push_back(nums[0]);
@@ -11,6 +15,28 @@ public:
             int b = arr2.size();
 
             if(arr1[a-1] > arr2[b-1]) arr1.push_back(nums[i]);
+            else arr2.push_back(nums[i]);
+        }
+
+        vector<int> res;
+        int i = 0, j = 0;
+
+        while(i < arr1.size()) { res.push_back(arr1[i]); i++; }
+        while(j < arr2.size()) { res.push_back(arr2[j]); j++; }
+        
+        return res;
+        */
+
+
+        // Simplified approach of above ...
+        int n = nums.size();
+        vector<int> arr1, arr2;
+        arr1.push_back(nums[0]);
+        arr2.push_back(nums[1]);
+
+        for(int i = 2; i < n; i++) {
+            
+            if(arr1.back() > arr2.back()) arr1.push_back(nums[i]);
             else arr2.push_back(nums[i]);
         }
 
