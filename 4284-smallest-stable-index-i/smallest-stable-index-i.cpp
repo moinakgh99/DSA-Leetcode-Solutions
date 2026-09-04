dbc86fea7078fb3e560ea52c9a1927347b5dc48e
Moinak Ghosh <216645880+moinakgh99@users.noreply.github.com>
2026-09-04 09:54:31 +0530

    Time: 3 ms (25.17%) | Memory: 30.3 MB (85.58%) - LeetSync

---
+++ 4284-smallest-stable-index-i/smallest-stable-index-i.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int firstStableIndex(vector<int>& nums, int k) {
+        int n = nums.size();
+
+        int ans = -1;
+        for(int i = 0; i < n; i++) {
+            int mxi = *max_element(nums.begin(), nums.begin() + i);
+            int mini = *min_element(nums.begin() + i, nums.end());
+
+            if( (mxi - mini) <= k) {
+                ans = i;
+                break;
+            } 
+        }
+
+        return ans;
+    }
+};
