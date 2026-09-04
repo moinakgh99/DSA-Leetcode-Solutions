6e26ff78a67fc0120d4e893864e183b6d7971821
Moinak Ghosh <216645880+moinakgh99@users.noreply.github.com>
2026-09-04 10:09:08 +0530

    Time: 3 ms (25.17%) | Memory: 31.5 MB (20.68%) - LeetSync

--- 4284-smallest-stable-index-i/smallest-stable-index-i.cpp
+++ 4284-smallest-stable-index-i/smallest-stable-index-i.cpp
@@ -3,6 +3,9 @@ public:
     int firstStableIndex(vector<int>& nums, int k) {
         int n = nums.size();
 
+        // Approach 1 : Using STLs ... O(n2) solution ..
+
+        /*
         int ans = -1;
         for(int i = 0; i < n; i++) {
             int mxi = *max_element(nums.begin(), nums.begin() + i);
@@ -15,5 +18,31 @@ public:
         }
 
         return ans;
+        */
+
+
+        // Approach 2 : Using prefix Sum ...
+        vector<int> maxi(n), mini(n);
+
+        int idx = -1;
+        int mx = INT_MIN, mn = INT_MAX;
+        for(int i = 0; i < n; i++) {
+            mx = max(mx, nums[i]);
+            maxi[i] = mx;
+        }
+
+        for(int i = n-1; i >= 0; i--) {
+            mn = min(mn, nums[i]);
+            mini[i] = mn;
+        }
+
+        for(int i = 0; i < n; i++) {
+            if((maxi[i] - mini[i]) <= k) {
+                idx = i;
+                break;
+            }
+        }
+
+        return idx;
     }
 };
