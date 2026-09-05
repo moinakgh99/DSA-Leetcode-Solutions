b9f8f122bb6f38fecdd1d2f52a57423212ae2a41
Moinak Ghosh <216645880+moinakgh99@users.noreply.github.com>
2026-09-05 18:31:19 +0530

    Time: 0 ms (100.00%) | Memory: 21.7 MB (76.15%) - LeetSync

--- 57-insert-interval/insert-interval.cpp
+++ 57-insert-interval/insert-interval.cpp
@@ -1,27 +1,26 @@
 class Solution {
 public:
-    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newIntervals) {
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
         int n = intervals.size();
         int i = 0;
         vector<vector<int>> res;
 
-        while(i < n && intervals[i][1] < newIntervals[0]) {
+        // left part
+        while(i < n &&  intervals[i][1] < newInterval[0]) {
             res.push_back(intervals[i]);
             i++;
         }
 
-        while(i < n && intervals[i][0] <= newIntervals[1]) {
-            newIntervals[0] = min(newIntervals[0], intervals[i][0]);
-            newIntervals[1] = max(newIntervals[1], intervals[i][1]);
+        // overlapping parts
+        while(i < n && intervals[i][0] <= newInterval[1]) {
+            newInterval[0] = min(newInterval[0], intervals[i][0]);
+            newInterval[1] = max(newInterval[1], intervals[i][1]);
             i++;
         }
+        res.push_back(newInterval);
 
-        res.push_back(newIntervals);
-
-        while(i < n) {
-            res.push_back(intervals[i]);
-            i++;
-        }
+        // right part
+        while(i < n) { res.push_back(intervals[i]); i++; }
 
         return res;
     }
