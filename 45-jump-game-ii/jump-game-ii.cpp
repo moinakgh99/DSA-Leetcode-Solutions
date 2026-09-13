534ec2cefa62d63951dacef5b8487c4ae0d8e657
Moinak Ghosh <216645880+moinakgh99@users.noreply.github.com>
2026-09-13 12:13:39 +0530

    Time: 0 ms (100.00%) | Memory: 20.6 MB (78.57%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -3,15 +3,12 @@ public:
     int jump(vector<int>& nums) {
         int n = nums.size();
 
-        if(n == 1) return 0;
-
-        int maxIdx = 0, jumps = 0;
+        int jumps = 0, maxIdx = 0;
         int end = 0;
 
         for(int i = 0; i < n-1; i++) {
+            maxIdx = max(i + nums[i], maxIdx);
 
-            maxIdx = max(maxIdx, i + nums[i]);
-            
             if(i == end) {
                 jumps++;
                 end = maxIdx;
