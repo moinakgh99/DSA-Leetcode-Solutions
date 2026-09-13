07482d3b1c35a7d7c51a79c027da1b739d1c2ec2
Moinak Ghosh <216645880+moinakgh99@users.noreply.github.com>
2026-09-13 12:41:13 +0530

    Time: 5 ms (24.08%) | Memory: 73.7 MB (6.48%) - LeetSync

--- 135-candy/candy.cpp
+++ 135-candy/candy.cpp
@@ -6,22 +6,18 @@ public:
         vector<int> left(n, 1), right(n, 1);
 
         for(int i = 1; i < n; i++) {
-            if(ratings[i] > ratings[i-1]) {
-                left[i] = left[i-1] + 1;
-            } 
+            if(ratings[i] > ratings[i-1]) left[i] = left[i-1] + 1;
         }
 
         for(int i = n-2; i >= 0; i--) {
-            if(ratings[i] > ratings[i+1]) {
-                right[i] = right[i+1] + 1;
-            } 
+            if(ratings[i] > ratings[i+1]) right[i] = right[i+1] + 1;
         }
 
-        int sum = 0;
+        int candies = 0;
         for(int i = 0; i < n; i++) {
-            sum += max(left[i], right[i]);
+            candies += max(left[i], right[i]);
         }
 
-        return sum;
+        return candies;
     }
 };
